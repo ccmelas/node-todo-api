3888df5ae4886753b8972b4419450515ce6eec13
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-05-02 17:15:36 +0100

    set up app for heroku

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,14 +6,14 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     console.log('Connected successfully');
     const db = client.db('TodoApp');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Another Next up to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err) return console.log('Insert Failed');
+    db.collection('Todos').insertOne({
+        text: 'Another Next up to do',
+        completed: false
+    }, (err, result) => {
+        if(err) return console.log('Insert Failed');
 
-    //     console.log('Success: ', result.ops);
-    // });
+        console.log('Success: ', result.ops);
+    });
 
     db.collection('Users').insertOne({
         name: 'Chiemela Chinedum',
